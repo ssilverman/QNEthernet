c3f15118045c41dfeb3790ee112537fb85bc10ac
Shawn Silverman <shawn@pobox.com>
2021-08-25 20:33:34 -0700

    Add HTTP client test to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@
 #include <lwip/netif.h>
 
 #include "Ethernet.h"
+#include "EthernetClient.h"
 #include "EthernetUDP.h"
 #include "OSC.h"
 
 qindesign::network::Ethernet eth{};
 qindesign::network::EthernetUDP udpIn{};
+qindesign::network::EthernetClient client{};
 
 static void netif_status_callback(struct netif *netif) {
   static char ip[IPADDR_STRLEN_MAX];
@@ -39,6 +41,11 @@ static void link_status_callback(struct netif *netif) {
                 netif_is_link_up(netif) ? "up" : "down");
 }
 
+void setupOSC();
+void setupHTTPClient();
+void loopOSC();
+void loopHTTPClient();
+
 void setup() {
   Serial.begin(115200);
   while (!Serial && millis() < 4000) {
@@ -50,21 +57,77 @@ void setup() {
   uint8_t mac[6];
   eth.macAddress(mac);
   Serial.printf("MAC = %02x:%02x:%02x:%02x:%02x:%02x\n",
-               mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
   eth.begin();
   netif_set_status_callback(netif_default, netif_status_callback);
   netif_set_link_callback(netif_default, link_status_callback);
+
+  // setupOSC();
+  setupHTTPClient();
+}
+
+void setupOSC() {
   udpIn.begin(8000);
+}
+
+void setupHTTPClient() {
 }
 
 void loop() {
   qindesign::network::Ethernet::loop();
 
+  // loopOSC();
+  loopHTTPClient();
+}
+
+void loopOSC() {
   int size = udpIn.parsePacket();
   if (size > 0) {
     unsigned char buf[size];
     udpIn.read(buf, size);
     printOSC(Serial, buf, size);
+  }
+}
+
+int httpClientState = 0;
+
+void loopHTTPClient() {
+  switch (httpClientState) {
+    case 0:
+      if (eth.localIP() != 0) {
+        if (client.connect(IPAddress{172, 217, 6, 46}, 80)) {  // example.com: 93, 184, 216, 34
+          Serial.println("connected");
+          client.print("GET /search?q=arduino HTTP/1.0\r\n");
+          // client.print("GET / HTTP/1.1\r\n");
+          // client.print("Host: example.com\r\n");
+          // client.print("Connection: close\r\n");
+          client.print("\r\n");
+          // client.flush();
+          httpClientState = 1;
+        } else {
+          Serial.println("connection failed");
+          httpClientState = 2;
+        }
+      }
+      break;
+
+    case 1: {
+      int avail = client.available();
+      if (avail > 0) {
+        for (int i = 0; i < avail; i++) {
+          Serial.print((char) client.read());
+        }
+      }
+      if (!client.connected()) {
+        Serial.println("disconnecting.");
+        client.stop();
+        httpClientState = 2;
+      }
+      break;
+    }
+
+    default:
+      break;
   }
 }
