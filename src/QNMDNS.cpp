7dcb79e60fc4ad856b08a0df2432fd630196dbb6
Shawn Silverman <shawn@pobox.com>
2021-08-31 19:48:14 -0700

    Add the ability to add TXT items to mDNS services

--- src/QNMDNS.cpp
+++ src/QNMDNS.cpp
@@ -6,6 +6,9 @@
 
 #include "QNMDNS.h"
 
+// C++ includes
+#include <algorithm>
+
 #include <lwip/apps/mdns.h>
 
 // Global definitions for Arduino
@@ -23,10 +26,24 @@ static constexpr size_t countof(const T (&)[N]) {
 // Recommended DNS TTL value, in seconds, per RFC 6762 "Multicast DNS".
 static constexpr uint32_t kTTL = 120;
 
-// static void srv_txt(struct mdns_service *service, void *txt_userdata) {
-//   err_t res = mdns_resp_add_service_txtitem(service, "path=/", 6);
-//   LWIP_ERROR("mdns add service txt failed\n", (res == ERR_OK), return );
-// }
+static void srv_txt(struct mdns_service *service, void *txt_userdata) {
+  if (txt_userdata == nullptr) {
+    return;
+  }
+
+  auto fn = reinterpret_cast<std::vector<String> (*)()>(txt_userdata);
+  std::vector<String> list = fn();
+  if (list.empty()) {
+    return;
+  }
+
+  for (const auto &item : list) {
+    const char *txt = item.c_str();
+    uint8_t len = std::min(item.length(), 63U);
+    err_t res = mdns_resp_add_service_txtitem(service, txt, len);
+    LWIP_ERROR("mdns add service txt failed\n", (res == ERR_OK), return );
+  }
+}
 
 bool MDNSClass::begin(const String &host) {
   netif_ = netif_default;
@@ -65,28 +82,33 @@ enum mdns_sd_proto toProto(const String &protocol) {
 }
 
 bool MDNSClass::addService(const String &type, const String &protocol,
-                           uint16_t port) {
+                           uint16_t port,
+                           std::vector<String> (*getTXTFunc)(void)) {
   if (netif_ == nullptr) {
     return false;
   }
 
-  int8_t slot =
-      mdns_resp_add_service(netif_, host_.c_str(), type.c_str(),
-                            toProto(protocol), port, kTTL,
-                            nullptr, nullptr);
+  int8_t slot = mdns_resp_add_service(netif_, host_.c_str(), type.c_str(),
+                                      toProto(protocol), port, kTTL, &srv_txt,
+                                      reinterpret_cast<void *>(getTXTFunc));
   if (slot < 0 || countof(slots_) <= static_cast<size_t>(slot)) {
+    if (slot >= 0) {
+      // Remove if the addition was successful but we couldn't add it
+      mdns_resp_del_service(netif_, slot);
+    }
     return false;
   }
-  auto tuple = std::make_tuple(type, protocol, port);
-  slots_[slot] = std::make_unique<decltype(tuple)>(tuple);
+
+  Service service{true, type, protocol, port, getTXTFunc};
+  slots_[slot] = service;
   return true;
 }
 
 int MDNSClass::findService(const String &type, const String &protocol,
                            uint16_t port) {
-  auto tuple = std::make_tuple(type, protocol, port);
+  Service service{true, type, protocol, port};
   for (size_t i = 0; i < countof(slots_); i++) {
-    if (slots_[i] != nullptr && *slots_[i] == tuple) {
+    if (slots_[i] == service) {
       return i;
     }
   }
@@ -105,11 +127,10 @@ bool MDNSClass::removeService(const String &type, const String &protocol,
   if (found < 0) {
     return false;
   }
-  bool retval = (mdns_resp_del_service(netif_, found) == ERR_OK);
-  if (retval) {
-    slots_[found] = nullptr;
+  if (static_cast<size_t>(found) < countof(slots_)) {
+    slots_[found].reset();
   }
-  return retval;
+  return (mdns_resp_del_service(netif_, found) == ERR_OK);
 }
 
 }  // namespace network
--- src/QNMDNS.h
+++ src/QNMDNS.h
@@ -8,11 +8,11 @@
 #define QNE_MDNS_H_
 
 // C++ includes
-#include <memory>
-#include <tuple>
+#include <vector>
 
 #include <WString.h>
 #include <lwip/netif.h>
+#include <lwip/apps/mdns_opts.h>
 
 namespace qindesign {
 namespace network {
@@ -33,13 +33,52 @@ class MDNSClass final {
 
   // Add a service. The protocol will be set to "_udp" for anything other than
   // "_tcp". The strings should have a "_" prefix. This uses the host name as
-  // the servie name.
-  bool addService(const String &type, const String &protocol, uint16_t port);
+  // the service name.
+  //
+  // The `getTXTFunc` parameter is the function associated with this service to
+  // retreive its TXT record parts. The entire TXT record can be a can be a
+  // maximum of 255 bytes, including length bytes, and each item in the record
+  // can be a maximum of 63 bytes. The function may be NULL, in which case no
+  // items are added.
+  bool addService(const String &type, const String &protocol, uint16_t port,
+                  std::vector<String> (*getTXTFunc)(void) = nullptr);
 
   // Remove a service.
   bool removeService(const String &type, const String &protocol, uint16_t port);
 
  private:
+  struct Service {
+    bool operator==(const Service &other) {
+      if (!valid || !other.valid) {
+        // Invalid services compare unequal
+        return false;
+      }
+      if (this == &other) {
+        return true;
+      }
+
+      // Don't compare the functions
+      return (type == other.type) &&
+             (protocol == other.protocol) &&
+             (port == other.port);
+    }
+
+    // Reset this service to be invalid and empty.
+    void reset() {
+      valid = false;
+      type = "";
+      protocol = "";
+      port = 0;
+      getTXTFunc = nullptr;
+    }
+
+    bool valid = false;
+    String type;
+    String protocol;
+    uint16_t port;
+    std::vector<String> (*getTXTFunc)(void);
+  };
+
   // Find the slot for the given service. This returns -1 if the service could
   // not be found.
   int findService(const String &type, const String &protocol, uint16_t port);
@@ -48,8 +87,7 @@ class MDNSClass final {
   String host_;
 
   // Holds information about all the slots.
-  std::unique_ptr<std::tuple<String, String, uint16_t>>
-      slots_[MDNS_MAX_SERVICES];
+  Service slots_[MDNS_MAX_SERVICES];
 };
 
 }  // namespace network
