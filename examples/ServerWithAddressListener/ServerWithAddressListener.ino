8c2e61bbe71d05a4e7f432bfc38c3b5821966531
Shawn Silverman <shawn@pobox.com>
2021-10-07 01:21:43 -0700

    Add Content-Type header to ServerWithAddressListener example

--- examples/ServerWithAddressListener/ServerWithAddressListener.ino
+++ examples/ServerWithAddressListener/ServerWithAddressListener.ino
@@ -148,6 +148,7 @@ void processClientData(ClientState &state) {
   printf("Sending to client: %u.%u.%u.%u\n", ip[0], ip[1], ip[2], ip[3]);
   state.client.write("HTTP/1.1 200 OK\r\n"
                      "Connection: close\r\n"
+                     "Content-Type: text/plain\r\n"
                      "\r\n"
                      "Hello, Client!\r\n");
   state.client.flush();
