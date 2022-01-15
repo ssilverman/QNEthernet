66d7be6bb44d3c225bafcb9ab04bcb6b116bf027
Shawn Silverman <shawn@pobox.com>
2022-01-14 16:54:47 -0800

    Change timestamp retrieval to use IEEE1588Timestamp

--- src/qnethernet/QNEthernetUDP.h
+++ src/qnethernet/QNEthernetUDP.h
@@ -29,6 +29,7 @@
 #include "qnethernet/internal/IPOpts.h"
 #include "qnethernet/internal/PrintfChecked.h"
 #include "qnethernet/internal/optional.h"
+#include "qnethernet/lwip_driver.h"
 
 namespace qindesign {
 namespace network {
@@ -265,7 +266,7 @@ class EthernetUDP : public UDP,
   // Gets the IEEE 1588 timestamp for the received packet and assigns it to the
   // `timestamp` parameter, if available. This returns whether the received
   // packet has a timestamp.
-  bool timestamp(uint32_t *timestamp) const;
+  bool timestamp(IEEE1588Timestamp &timestamp) const;
 
  private:
   // Packet holds packet data. destAddr is unused for outgoing packets.
@@ -280,7 +281,7 @@ class EthernetUDP : public UDP,
 
     // IEEE 1588
     volatile bool hasTimestamp = false;
-    volatile uint32_t timestamp = 0;
+    IEEE1588Timestamp timestamp{0, 0};
 
     // Clears all the data.
     void clear();
