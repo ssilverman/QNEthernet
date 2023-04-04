b368dc3393cfb4cf95d06073948319ec7a745b13
Shawn Silverman <shawn@pobox.com>
2023-04-04 14:07:38 -0700

    Update some copyright dates to include 2023

--- examples/FixedWidthServer/FixedWidthServer.ino
+++ examples/FixedWidthServer/FixedWidthServer.ino
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: (c) 2021-2022 Shawn Silverman <shawn@pobox.com>
+// SPDX-FileCopyrightText: (c) 2021-2023 Shawn Silverman <shawn@pobox.com>
 // SPDX-License-Identifier: MIT
 
 // FixedWidthServer demonstrates how to serve a protocol having a
