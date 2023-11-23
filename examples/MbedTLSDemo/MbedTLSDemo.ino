a16ed910fc1c85a71bbaedda136875ef1a06c5c9
Shawn Silverman <shawn@pobox.com>
2023-11-23 11:43:08 -0800

    Move MbedTLSDemo example's adapter function implementations to their own file

--- examples/MbedTLSDemo/MbedTLSDemo.ino
+++ examples/MbedTLSDemo/MbedTLSDemo.ino
@@ -43,66 +43,6 @@ EthernetClient client;
 bool disconnectedPrintLatch = false;  // Print "disconnected" only once
 size_t dataCount = 0;
 
-// Mbed TLS adapter functions
-
-std::function<bool(const ip_addr_t *, uint16_t)> qnethernet_mbedtls_is_tls =
-    [](const ip_addr_t *ipaddr, uint16_t port) {
-      // Given the IP address and port, determine if the connection
-      // needs to use TLS
-      printf("[[qnethernet_mbedtls_is_tls(%s, %" PRIu16 "): %s]]\r\n",
-             (ipaddr == nullptr) ? nullptr : ipaddr_ntoa(ipaddr), port,
-             (ipaddr == nullptr) ? "Listen" : "Connect");
-
-      if (port == 443) {
-        if (ipaddr == nullptr) {
-          printf("qnethernet_mbedtls_is_tls: creating server config...\r\n");
-        } else {
-          printf("qnethernet_mbedtls_is_tls: creating client config...\r\n");
-        }
-        return true;
-      }
-      return false;
-    };
-
-std::function<void(const ip_addr_t &, uint16_t, const uint8_t *&, size_t &)>
-    qnethernet_altcp_tls_client_cert =
-        [](const ip_addr_t &ipaddr, uint16_t port,
-           const uint8_t *&cert, size_t &cert_len) {
-          printf("[[qnethernet_altcp_tls_client_cert(%s, %" PRIu16 ")]]"
-                 " No certificate\r\n",
-                 ipaddr_ntoa(&ipaddr), port);
-          // To set certificate data, set:
-          // cert = pointer to certificate data
-          // cert_len = the certificate length
-          // You can optionally use the IP address and port to
-          // determine which data to use
-        };
-
-std::function<uint8_t(uint16_t)> qnethernet_altcp_tls_server_cert_count =
-    [](uint16_t port) {
-      // The number of times qnethernet_altcp_tls_server_cert will
-      // get called
-      return 0;
-    };
-
-std::function<void(uint16_t, uint8_t,
-                   const uint8_t *&, size_t &,
-                   const uint8_t *&, size_t &,
-                   const uint8_t *&, size_t &)>
-    qnethernet_altcp_tls_server_cert =
-        [](uint16_t port, uint8_t index,
-           const uint8_t *&privkey,      size_t &privkey_len,
-           const uint8_t *&privkey_pass, size_t &privkey_pass_len,
-           const uint8_t *&cert,         size_t &cert_len) {
-          printf("[[qnethernet_altcp_tls_server_cert(port %" PRIu16 ","
-                 " index %" PRIu8 ")]]\r\n",
-                 port, index);
-          // To set certificate data, simply set the values;
-          // they're all references
-          // You can optionally use the port and certificate index to
-          // determine which data to use
-        };
-
 // Program setup.
 void setup() {
   Serial.begin(115200);
---
+++ examples/MbedTLSDemo/adapter_functions.cpp
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: (c) 2023 Shawn Silverman <shawn@pobox.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// adapter_functions.cpp implements the Mbed TLS adapter functions.
+//
+// This file is part of the QNEthernet library.
+
+#include <cinttypes>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <functional>
+
+#include <lwip/ip_addr.h>
+#include <lwip/opt.h>
+
+#if LWIP_ALTCP_TLS && LWIP_ALTCP_TLS_MBEDTLS
+
+std::function<bool(const ip_addr_t *, uint16_t)> qnethernet_mbedtls_is_tls =
+    [](const ip_addr_t *ipaddr, uint16_t port) {
+      // Given the IP address and port, determine if the connection
+      // needs to use TLS
+      printf("[[qnethernet_mbedtls_is_tls(%s, %" PRIu16 "): %s]]\r\n",
+             (ipaddr == nullptr) ? nullptr : ipaddr_ntoa(ipaddr), port,
+             (ipaddr == nullptr) ? "Listen" : "Connect");
+
+      if (port == 443) {
+        if (ipaddr == nullptr) {
+          printf("qnethernet_mbedtls_is_tls: creating server config...\r\n");
+        } else {
+          printf("qnethernet_mbedtls_is_tls: creating client config...\r\n");
+        }
+        return true;
+      }
+      return false;
+    };
+
+std::function<void(const ip_addr_t &, uint16_t, const uint8_t *&, size_t &)>
+    qnethernet_altcp_tls_client_cert =
+        [](const ip_addr_t &ipaddr, uint16_t port,
+           const uint8_t *&cert, size_t &cert_len) {
+          printf("[[qnethernet_altcp_tls_client_cert(%s, %" PRIu16 ")]]"
+                 " No certificate\r\n",
+                 ipaddr_ntoa(&ipaddr), port);
+          // To set certificate data, set:
+          // cert = pointer to certificate data
+          // cert_len = the certificate length
+          // You can optionally use the IP address and port to
+          // determine which data to use
+        };
+
+std::function<uint8_t(uint16_t)> qnethernet_altcp_tls_server_cert_count =
+    [](uint16_t port) {
+      // The number of times qnethernet_altcp_tls_server_cert will
+      // get called
+      return 0;
+    };
+
+std::function<void(uint16_t, uint8_t,
+                   const uint8_t *&, size_t &,
+                   const uint8_t *&, size_t &,
+                   const uint8_t *&, size_t &)>
+    qnethernet_altcp_tls_server_cert =
+        [](uint16_t port, uint8_t index,
+           const uint8_t *&privkey,      size_t &privkey_len,
+           const uint8_t *&privkey_pass, size_t &privkey_pass_len,
+           const uint8_t *&cert,         size_t &cert_len) {
+          printf("[[qnethernet_altcp_tls_server_cert(port %" PRIu16 ","
+                 " index %" PRIu8 ")]]\r\n",
+                 port, index);
+          // To set certificate data, simply set the values;
+          // they're all references
+          // You can optionally use the port and certificate index to
+          // determine which data to use
+        };
+
+#endif  // LWIP_ALTCP_TLS && LWIP_ALTCP_TLS_MBEDTLS
