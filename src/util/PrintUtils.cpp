7f38609628d7aa2767835a18d4936a83d7bb4054
Shawn Silverman <shawn@pobox.com>
2024-03-03 09:33:50 -0800

    Update a copyright date to include 2024

--- src/util/PrintUtils.cpp
+++ src/util/PrintUtils.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: (c) 2022-2023 Shawn Silverman <shawn@pobox.com>
+// SPDX-FileCopyrightText: (c) 2022-2024 Shawn Silverman <shawn@pobox.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 // PrintUtils.cpp implements the Print utility functions.
