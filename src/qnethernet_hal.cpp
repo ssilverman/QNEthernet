7f30ac80b86027f4a84593e94c2d3ced970ed1ee
Shawn Silverman <shawn@pobox.com>
2024-03-30 22:58:47 -0700

    hal: Add qnethernet_hal_get_system_mac_address()
    Copied from mac_tools sources.

--- src/qnethernet_hal.cpp
+++ src/qnethernet_hal.cpp
@@ -19,7 +19,21 @@
 #include <Arduino.h>  // For Serial, noInterrupts(), interrupts()
 #include <Print.h>
 
+// Processor-specific include
+#if defined(TEENSYDUINO)
+#if defined(__IMXRT1062__)
+#include <imxrt.h>
+#elif defined(ARDUINO_TEENSY30) || \
+      defined(ARDUINO_TEENSY32) || defined(ARDUINO_TEENSY31) || \
+      defined(ARDUINO_TEENSYLC) || \
+      defined(ARDUINO_TEENSY35) || \
+      defined(ARDUINO_TEENSY36)
+#include <kinetis.h>
+#endif  // Teensy type
+#endif  // defined(TEENSYDUINO)
+
 #include "lwip/arch.h"
+#include "lwip/prot/ethernet.h"
 #include "security/RandomDevice.h"
 
 // --------------------------------------------------------------------------
@@ -190,6 +204,80 @@ void qnethernet_hal_disable_interrupts() {
 [[gnu::weak]]
 void qnethernet_hal_enable_interrupts() {
   interrupts();
+}
+
+}  // extern "C"
+
+// --------------------------------------------------------------------------
+//  MAC Address
+// --------------------------------------------------------------------------
+
+extern "C" {
+
+#if !defined(TEENSYDUINO)
+static const uint8_t kDefaultMACAddress[ETH_HWADDR_LEN] = {
+    QNETHERNET_DEFAULT_MAC_ADDRESS,
+};
+#endif  // !defined(TEENSYDUINO)
+
+// Gets the system MAC address. This will either be some platform-specific value
+// or a predefined value.
+void qnethernet_hal_get_system_mac_address(uint8_t mac[ETH_HWADDR_LEN]) {
+  if (mac == nullptr) {
+    return;
+  }
+
+#if defined(TEENSYDUINO) && defined(__IMXRT1062__)
+  uint32_t m1 = HW_OCOTP_MAC1;
+  uint32_t m2 = HW_OCOTP_MAC0;
+  mac[0] = m1 >> 8;
+  mac[1] = m1 >> 0;
+  mac[2] = m2 >> 24;
+  mac[3] = m2 >> 16;
+  mac[4] = m2 >> 8;
+  mac[5] = m2 >> 0;
+#elif defined(ARDUINO_TEENSY30) || \
+      defined(ARDUINO_TEENSY32) || defined(ARDUINO_TEENSY31) || \
+      defined(ARDUINO_TEENSYLC)
+  // usb_desc.c:usb_init_serialnumber()
+  __disable_irq();
+  FTFL_FSTAT = FTFL_FSTAT_RDCOLERR | FTFL_FSTAT_ACCERR | FTFL_FSTAT_FPVIOL;
+  FTFL_FCCOB0 = 0x41;
+  FTFL_FCCOB1 = 15;
+  FTFL_FSTAT = FTFL_FSTAT_CCIF;
+  while (!(FTFL_FSTAT & FTFL_FSTAT_CCIF)) {
+    // Wait
+  }
+  uint32_t num = *(uint32_t *)&FTFL_FCCOB7;
+  __enable_irq();
+  mac[0] = 0x04;
+  mac[1] = 0xE9;
+  mac[2] = 0xE5;
+  mac[3] = num >> 16;
+  mac[4] = num >> 8;
+  mac[5] = num;
+#elif defined(ARDUINO_TEENSY35) || defined(ARDUINO_TEENSY36)
+  // usb_desc.c:usb_init_serialnumber()
+  __disable_irq();
+  kinetis_hsrun_disable();
+  FTFL_FSTAT = FTFL_FSTAT_RDCOLERR | FTFL_FSTAT_ACCERR | FTFL_FSTAT_FPVIOL;
+  *(uint32_t *)&FTFL_FCCOB3 = 0x41070000;
+  FTFL_FSTAT = FTFL_FSTAT_CCIF;
+  while (!(FTFL_FSTAT & FTFL_FSTAT_CCIF)) {
+    // Wait
+  }
+  uint32_t num = *(uint32_t *)&FTFL_FCCOBB;
+  kinetis_hsrun_enable();
+  __enable_irq();
+  mac[0] = 0x04;
+  mac[1] = 0xE9;
+  mac[2] = 0xE5;
+  mac[3] = num >> 16;
+  mac[4] = num >> 8;
+  mac[5] = num;
+#else
+  memcpy(mac, kDefaultMACAddress, ETH_HWADDR_LEN);
+#endif  // Board type
 }
 
 }  // extern "C"
