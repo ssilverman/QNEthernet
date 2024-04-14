1386396f017fb41ebb61b9ced64529378d0033e9
Shawn Silverman <shawn@pobox.com>
2024-04-13 21:59:23 -0700

    Update examples to compile with C++11

--- examples/FixedWidthServer/FixedWidthServer.ino
+++ examples/FixedWidthServer/FixedWidthServer.ino
@@ -20,7 +20,7 @@ using namespace qindesign::network;
 //  Configuration
 // --------------------------------------------------------------------------
 
-constexpr uint32_t kDHCPTimeout = 15'000;  // 15 seconds
+constexpr uint32_t kDHCPTimeout = 15000;  // 15 seconds
 
 constexpr uint16_t kServerPort = 5000;
 
@@ -134,9 +134,10 @@ void loop() {
 
   // Clean up all the closed clients
   size_t size = clients.size();
-  clients.erase(std::remove_if(clients.begin(), clients.end(),
-                               [](const auto &state) { return state.closed; }),
-                clients.end());
+  clients.erase(
+      std::remove_if(clients.begin(), clients.end(),
+                     [](const ClientState &state) { return state.closed; }),
+      clients.end());
   if (clients.size() != size) {
     printf("Client count: %zu\r\n", clients.size());
   }
