5d60b959aef58cfe183a2ebc4f7c8e4ab6fdaedb
Shawn Silverman <shawn@pobox.com>
2024-10-27 12:17:05 -0500

    MDNSClass: Return 'netifAdded' in operator bool() instead of the nullptr check

--- src/QNMDNS.h
+++ src/QNMDNS.h
@@ -122,7 +122,7 @@ class MDNSClass final {
 
   // Returns whether mDNS has been started.
   explicit operator bool() const {
-    return netif_ != nullptr;
+    return netifAdded;
   }
 
   // The following functions are for periodically re-announcing the services.
