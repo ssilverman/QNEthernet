2a41d84143150096203e7c4fca1fd0b2005a831d
Shawn Silverman <shawn@pobox.com>
2024-12-20 10:56:43 -0800

    MbedTLSDemo: Update for MbedTLSClient and mbedtls-3.6.2

--- examples/MbedTLSDemo/MbedTLSDemo.ino
+++ examples/MbedTLSDemo/MbedTLSDemo.ino
@@ -1,34 +1,25 @@
 // SPDX-FileCopyrightText: (c) 2023-2026 Shawn Silverman <shawn@pobox.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-// MbedTLSDemo shows how to use Mbed TLS 2.x.x.
+// MbedTLSDemo shows how to use Mbed TLS 3.6.x.
 //
 // Prerequisites:
-// 1. Enable the following lwIP options (in lwipopts.h or the project
-//    build options):
-//    1. LWIP_ALTCP
-//    2. LWIP_ALTCP_TLS
-//    3. LWIP_ALTCP_TLS_MBEDTLS
-// 2. Enable the QNETHERNET_ALTCP_TLS_ADAPTER option.
-//    (In the qnethernet_opts.h library file or the project
-//    build options.)
-// 3. Install the latest 2.x.x version of Mbed TLS. As of this
-//    writing, the current version is 2.28.9. In order to use this
+// 1. Install the latest 3.6.x version of Mbed TLS. As of this
+//    writing, the current version is 3.6.2. In order to use this
 //    example program in the Arduino IDE, Mbed TLS needs to be
 //    installed as a library. Please see the README for more
 //    information.
-// 4. Update src/mbedtls/config.h in the Mbed TLS library with the
-//    desired options. A sample config is in this project. Simply
+// 2. Update src/mbedtls/mbedtls_config.h in the Mbed TLS library with
+//    the desired options. A sample config is in this project. Simply
 //    cut & paste.
 //
 // This file is part of the QNEthernet library.
 
-#include <QNEthernet.h>
-#if LWIP_ALTCP
-#if LWIP_ALTCP_TLS && LWIP_ALTCP_TLS_MBEDTLS
+#include <cstring>
+
 #include <mbedtls.h>  // Must be included before referencing other mbedtls headers
-#endif  // LWIP_ALTCP_TLS && LWIP_ALTCP_TLS_MBEDTLS
-#endif  // LWIP_ALTCP
+#include <MbedTLSClient.h>
+#include <QNEthernet.h>
 
 using namespace qindesign::network;
 
@@ -56,6 +47,7 @@ constexpr uint16_t kPort = 443;   // HTTPS generally uses port 443
 namespace {  // Internal linkage section
 
 EthernetClient client;
+MbedTLSClient tlsclient{client};
 
 bool disconnectedPrintLatch = false;  // Print "disconnected" only once
 size_t dataCount = 0;
@@ -102,12 +94,14 @@ void setup() {
 
   // Connect and send the request
   printf("Connecting and sending request...\r\n");
-  if (!client.connect(kHost, kPort)) {
+  if (!tlsclient.connect(kHost, kPort)) {
     printf("Failed to connect\r\n");
     disconnectedPrintLatch = true;
   } else {
-    client.writeFully(kRequest);
-    client.flush();
+    util::writeFully(tlsclient, reinterpret_cast<const uint8_t *>(kRequest),
+                     std::strlen(kRequest),
+                     []() { return !static_cast<bool>(tlsclient); });
+    tlsclient.flush();
     dataCount = 0;
     printf("[Awaiting response...]\r\n");
   }
@@ -116,12 +110,12 @@ void setup() {
 // Main program loop.
 void loop() {
   // Read the response
-  if (client.connected()) {
-    int avail = client.available();
+  if (tlsclient.connected()) {
+    int avail = tlsclient.available();
     if (avail > 0) {
       dataCount += avail;
       for (int i = 0; i < avail; ++i) {
-        putc(client.read(), stdout);
+        putc(tlsclient.read(), stdout);
       }
     }
   } else {
