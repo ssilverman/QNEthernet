7fa59a08ffe34c33ca806525d6d28261329a7a03
Shawn Silverman <shawn@pobox.com>
2024-12-26 17:14:34 -0800

    MbedTLSClinet: Call deinit() if handshake(() returns false

--- src/MbedTLSClient.cpp
+++ src/MbedTLSClient.cpp
@@ -264,10 +264,12 @@ bool MbedTLSClient::handshake(const char *const hostname, const bool wait) {
     return false;
   }
   if (mbedtls_ssl_setup(&ssl_, &conf_) != 0) {
+    deinit();
     return false;
   }
   if (!isServer_) {
     if (mbedtls_ssl_set_hostname(&ssl_, hostname) != 0) {
+      deinit();
       return false;
     }
   }
@@ -286,6 +288,7 @@ bool MbedTLSClient::handshake(const char *const hostname, const bool wait) {
 
     if (handshakeTimeout_ != 0 &&
         qnethernet_hal_millis() - startTime >= handshakeTimeout_) {
+      deinit();
       return false;
     }
   }
