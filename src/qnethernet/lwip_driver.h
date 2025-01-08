8e9cd724f37d2e118f83525acf6425da7ac67655
Shawn Silverman <shawn@pobox.com>
2025-01-08 09:25:39 -0800

    driver: Add note that MTU or max-frame-len may change over time

--- src/qnethernet/lwip_driver.h
+++ src/qnethernet/lwip_driver.h
@@ -122,12 +122,12 @@ namespace driver {
 // is called.
 void get_capabilities(DriverCapabilities* dc);
 
-// Returns the MTU.
+// Returns the MTU. This may change over time.
 ATTRIBUTE_NODISCARD
 size_t get_mtu(void);
 
 // Returns the maximum frame length. This does not include the 4-byte FCS (frame
-// check sequence).
+// check sequence). This may change over time.
 ATTRIBUTE_NODISCARD
 size_t get_max_frame_len(void);
 
