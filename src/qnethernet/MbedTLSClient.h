56a60f0a3dcbe2d0cf1e26f88a60fb700b1e48d0
Shawn Silverman <shawn@pobox.com>
2025-01-12 11:37:15 -0800

    Update some copyright dates to include 2025

--- src/qnethernet/MbedTLSClient.h
+++ src/qnethernet/MbedTLSClient.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: (c) 2024 Shawn Silverman <shawn@pobox.com>
+// SPDX-FileCopyrightText: (c) 2024-2025 Shawn Silverman <shawn@pobox.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 // MbedTLSClient.h defines a Client wrapper for TLS connections that uses the
