028ed2083dd11b564ac904e80ff6edfadcf527d7
Shawn Silverman <shawn@pobox.com>
2025-03-30 15:04:21 -0700

    teensymm: WIP

---
+++ src/qnethernet/drivers/driver_teensymm.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: (c) 2021-2025 Shawn Silverman <shawn@pobox.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// driver_teensymm.h defines Teensy MicroMod-specific things.
+// This file is part of the QNEthernet library.
+
+#pragma once
+
+#define MTU 1500
+#define MAX_FRAME_LEN 1522
+
+// lwIP options
+
+// ARP options
+#define ETH_PAD_SIZE 2 /* 0 */
+
+// Checksum options
+#define CHECKSUM_GEN_IP 0   /* 1 */
+#define CHECKSUM_GEN_UDP 0  /* 1 */
+#define CHECKSUM_GEN_TCP 0  /* 1 */
+#define CHECKSUM_GEN_ICMP 0 /* 1 */
+// #define CHECKSUM_GEN_ICMP6   1
+#define CHECKSUM_CHECK_IP 0   /* 1 */
+#define CHECKSUM_CHECK_UDP 0  /* 1 */
+#define CHECKSUM_CHECK_TCP 0  /* 1 */
+#define CHECKSUM_CHECK_ICMP 0 /* 1 */
+// #define CHECKSUM_CHECK_ICMP6 1
