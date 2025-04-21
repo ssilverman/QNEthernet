c0bb4fa27633c57d166463204a613a25ba8b738c
Shawn Silverman <shawn@pobox.com>
2025-04-21 12:15:21 -0700

    MbedTLSDemo: Update mbedtls_hardware_poll()
    Use qnethernet_hal_fill_entropy() instead of preprocessor checks.

--- examples/MbedTLSDemo/mbedtls_entropy.c
+++ examples/MbedTLSDemo/mbedtls_entropy.c
@@ -10,47 +10,26 @@
 #if LWIP_ALTCP && LWIP_ALTCP_TLS
 
 #include <lwip/apps/altcp_tls_mbedtls_opts.h>
-#include <lwip/arch.h>
 
 #if LWIP_ALTCP_TLS_MBEDTLS
 
 // C includes
 #include <stddef.h>
 
-#if (defined(TEENSYDUINO) && defined(__IMXRT1062__)) && \
-    !QNETHERNET_USE_ENTROPY_LIB
+size_t qnethernet_hal_fill_entropy(void *buf, size_t size);
 
-#include <qnethernet/security/entropy.h>
-
+// Gets entropy for MbedTLS.
 int mbedtls_hardware_poll(void *data,
-                          unsigned char *output, size_t len, size_t *olen) {
-  LWIP_UNUSED_ARG(data);
+                          unsigned char *output, size_t len,
+                          size_t *olen) {
+  (void)data;
 
-  size_t out = trng_data(output, len);
+  size_t filled = qnethernet_hal_fill_entropy(output, len);
   if (olen != NULL) {
-    *olen = out;
+    *olen = filled;
   }
-  return 0;
+  return 0;  // Success
 }
-
-#else
-
-#include <lwip/arch.h>
-
-int mbedtls_hardware_poll(void *data,
-                          unsigned char *output, size_t len, size_t *olen) {
-  LWIP_UNUSED_ARG(data);
-
-  for (size_t i = len; i-- > 0; ) {
-    *(output++) = LWIP_RAND();
-  }
-  if (olen != NULL) {
-    *olen = len;
-  }
-  return 0;
-}
-
-#endif  // Which implementation
 
 #endif  // LWIP_ALTCP_TLS_MBEDTLS
 #endif  // LWIP_ALTCP && LWIP_ALTCP_TLS
