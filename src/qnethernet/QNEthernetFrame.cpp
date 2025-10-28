ssilverman/QNEthernet

commit 045d97411ae153c3cadb3b9520cfef89761a033d

Use more default member initializers instead of ctor initializers
C.48: Prefer default member initializers to member initializers in
constructors for constant initializers
https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rc-in-class-initializer

src/qnethernet/QNEthernetFrame.cpp
@@ -82,15 +82,7 @@ err_t EthernetFrameClass::recvFunc(struct pbuf *const p,
   return ERR_OK;
 }
 
-FLASHMEM EthernetFrameClass::EthernetFrameClass()
-    : inBuf_(1),
-      inBufTail_(0),
-      inBufHead_(0),
-      inBufSize_(0),
-      framePos_(-1),
-      hasOutFrame_(false),
-      droppedReceiveCount_(0),
-      totalReceiveCount_(0) {
+FLASHMEM EthernetFrameClass::EthernetFrameClass() {
   setReceiveQueueCapacity(1);
 }
 

src/qnethernet/QNEthernetFrame.h
@@ -236,22 +236,22 @@ class EthernetFrameClass final : public Stream, public internal::PrintfChecked {
   bool isAvailable() const;
 
   // Received frame; updated every time one is received
-  std::vector<Frame> inBuf_;  // Holds received frames
-  size_t inBufTail_;
-  size_t inBufHead_;
-  size_t inBufSize_;
+  std::vector<Frame> inBuf_(1);  // Holds received frames
+  size_t inBufTail_ = 0;
+  size_t inBufHead_ = 0;
+  size_t inBufSize_ = 0;
 
   // Frame being processed by the caller
   Frame frame_;   // Holds the frame being read
-  int framePos_;  // -1 if not currently reading a frame
+  int framePos_ = -1;  // -1 if not currently reading a frame
 
   // Outgoing frames
-  bool hasOutFrame_;
+  bool hasOutFrame_ = false;
   Frame outFrame_;
 
   // Stats
-  uint32_t droppedReceiveCount_;
-  uint32_t totalReceiveCount_;
+  uint32_t droppedReceiveCount_ = 0;
+  uint32_t totalReceiveCount_   = 0;
 
   friend class StaticInit<EthernetFrameClass>;
   friend err_t ::unknown_eth_protocol(struct pbuf *p, struct netif *netif);

examples/PixelPusherServer/CircularBuffer.h
@@ -16,10 +16,7 @@ class CircularBuffer {
  public:
   explicit CircularBuffer(size_t capacity)
       : capacity_(capacity),
-        buf_{std::make_unique<T[]>(capacity)},
-        size_(0),
-        head_(0),
-        tail_(0) {}
+        buf_{std::make_unique<T[]>(capacity)} {}
 
   bool empty() const {
     return size_ == 0;
@@ -79,7 +76,7 @@ class CircularBuffer {
 
   std::unique_ptr<T[]> buf_;
 
-  size_t size_;
-  size_t head_;
-  size_t tail_;
+  size_t size_ = 0;
+  size_t head_ = 0;
+  size_t tail_ = 0;
 };

src/QNEthernet.h
@@ -476,22 +476,22 @@ class EthernetClass final {
   // Driver configuration
   DriverCapabilities driverCapabilities_;
 
-  int chipSelectPin_;
+  int chipSelectPin_ = -1;
 
-  uint32_t lastPollTime_;
+  uint32_t lastPollTime_ = 0;
 
-  bool hasMAC_;
+  bool hasMAC_ = false;
   uint8_t mac_[kMACAddrSize];
 #if LWIP_NETIF_HOSTNAME
-  char hostname_[256];  // Empty means no hostname
+  char hostname_[256]{QNETHERNET_DEFAULT_HOSTNAME};  // Empty means no hostname
 #endif  // LWIP_NETIF_HOSTNAME
-  struct netif *netif_;
-  char ifName_[6];  // Two letters plus up to 3-digit number
+  struct netif *netif_ = nullptr;
+  char ifName_[6]{'\0'};  // Two letters plus up to 3-digit number
 
 #if LWIP_DHCP
-  bool dhcpEnabled_;
-  bool dhcpDesired_;  // Whether the user wants static or dynamic IP
-  bool dhcpActive_;
+  bool dhcpEnabled_ = true;
+  bool dhcpDesired_ = false;  // Whether the user wants static or dynamic IP
+  bool dhcpActive_  = false;
 #endif  // LWIP_DHCP
 
   // Callbacks

src/qnethernet/QNEthernet.cpp
@@ -123,23 +123,7 @@ void EthernetClass::netifEventFunc(
   }
 }
 
-FLASHMEM EthernetClass::EthernetClass()
-    : chipSelectPin_(-1),
-      lastPollTime_(0),
-      hasMAC_(false),
-#if LWIP_NETIF_HOSTNAME
-      hostname_{QNETHERNET_DEFAULT_HOSTNAME},
-#endif  // LWIP_NETIF_HOSTNAME
-      netif_(nullptr),
-      ifName_{0}
-#if LWIP_DHCP
-      ,
-      dhcpEnabled_(true),
-      dhcpDesired_(false),
-      dhcpActive_(false)
-#endif  // LWIP_DHCP
-{
-}
+FLASHMEM EthernetClass::EthernetClass() {}
 
 FLASHMEM EthernetClass::~EthernetClass() {
   end();

src/qnethernet/QNEthernetClient.cpp
@@ -46,10 +46,7 @@ EthernetClient::EthernetClient() : EthernetClient(nullptr) {}
 
 EthernetClient::EthernetClient(
     const std::shared_ptr<internal::ConnectionHolder> conn)
-    : connTimeout_(1000),
-      pendingConnect_(false),
-      connTimeoutEnabled_(true),
-      conn_(conn) {}
+    : conn_(conn) {}
 
 EthernetClient::~EthernetClient() = default;
     // Questionable not to call close(), but copy semantics demand that we don't

src/qnethernet/QNEthernetClient.h
@@ -272,9 +272,9 @@ class EthernetClient : public internal::ClientEx,
   bool getAddrInfo(bool local, ip_addr_t *addr, u16_t *port);
 
   // Connection state
-  uint32_t connTimeout_;
-  bool pendingConnect_;
-  bool connTimeoutEnabled_;
+  uint32_t connTimeout_    = 1000;
+  bool pendingConnect_     = false;
+  bool connTimeoutEnabled_ = true;
 
   std::shared_ptr<internal::ConnectionHolder> conn_;
       // If this has not been stopped then conn_ might still be non-NULL, so we

src/qnethernet/QNEthernetUDP.cpp
@@ -91,18 +91,7 @@ void EthernetUDP::recvFunc(void *const arg, struct udp_pcb *const pcb,
 EthernetUDP::EthernetUDP() : EthernetUDP(1) {}
 
 EthernetUDP::EthernetUDP(const size_t capacity)
-    : pcb_(nullptr),
-      listening_(false),
-      listenReuse_(false),
-      listeningMulticast_(false),
-      inBuf_(std::max(capacity, size_t{1})),
-      inBufTail_(0),
-      inBufHead_(0),
-      inBufSize_(0),
-      packetPos_(-1),
-      hasOutPacket_(false),
-      droppedReceiveCount_(0),
-      totalReceiveCount_(0) {}
+    : inBuf_(std::max(capacity, size_t{1})) {}
 
 EthernetUDP::~EthernetUDP() {
   stop();

commit e406178cc87f746aad3126d6f05b2262c5d22ecb

EthernetFrameClass: Fix compile error
The bug was introduced in 045d97411.

src/qnethernet/QNEthernetFrame.cpp
@@ -82,9 +82,8 @@ err_t EthernetFrameClass::recvFunc(struct pbuf* const p,
   return ERR_OK;
 }
 
-FLASHMEM EthernetFrameClass::EthernetFrameClass() {
-  setReceiveQueueCapacity(1);
-}
+FLASHMEM EthernetFrameClass::EthernetFrameClass()
+    : inBuf_(1) {}
 
 void EthernetFrameClass::Frame::clear() {
   data.clear();

src/qnethernet/QNEthernetFrame.h
@@ -237,7 +237,7 @@ class EthernetFrameClass final : public Stream, public internal::PrintfChecked {
   bool isAvailable() const;
 
   // Received frame; updated every time one is received
-  std::vector<Frame> inBuf_(1);  // Holds received frames
+  std::vector<Frame> inBuf_;  // Holds received frames
   size_t inBufTail_ = 0;
   size_t inBufHead_ = 0;
   size_t inBufSize_ = 0;