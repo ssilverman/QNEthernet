5198fc3c0ec53df30cdeddb70e27639387c8d515
Shawn Silverman <shawn@pobox.com>
2025-11-05 14:35:48 -0800

    Update the library version to 0.34.0-snapshot

--- src/QNEthernet.h
+++ src/QNEthernet.h
@@ -60,7 +60,7 @@ class EthernetClass final {
 
   // Returns a string containing the library version number.
   static const char* libraryVersion() {
-    return "0.33.0";
+    return "0.34.0-snapshot";
   }
 
   // Returns the maximum number of multicast groups. Note that mDNS will use
