e73daa434bfbf0cb880ddc90969729cda1b765db
Shawn Silverman <shawn@pobox.com>
2025-11-15 16:00:21 -0800

    StaticInit: Declare move operataions as 'delete', per rule of five

--- src/qnethernet/StaticInit.h
+++ src/qnethernet/StaticInit.h
@@ -41,10 +41,12 @@ struct StaticInit {
     }
   }
 
-  // Rule of three
+  // Rule of five
   // See: https://en.cppreference.com/w/cpp/language/rule_of_three.html
   StaticInit(const StaticInit&) = delete;
+  StaticInit(StaticInit&&) = delete;
   StaticInit& operator=(const StaticInit&) = delete;
+  StaticInit& operator=(StaticInit&&) = delete;
 
   static int nifty_counter;
   alignas(T) static unsigned char buf[sizeof(T)];
