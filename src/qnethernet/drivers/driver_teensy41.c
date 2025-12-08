a8bd8b95100afb67f00528b1808248c9efee94ac
Shawn Silverman <shawn@pobox.com>
2025-12-08 11:47:45 -0800

    driver_teensy41: Rename ieee1588_init(void) to driver_ieee1588_init(void)

--- src/qnethernet/drivers/driver_teensy41.c
+++ src/qnethernet/drivers/driver_teensy41.c
@@ -1381,7 +1381,7 @@ void driver_reset_phy(void) {
 
 #define TIMER_CHANNEL_COUNT 4
 
-void ieee1588_init(void) {
+void driver_ieee1588_init(void) {
   ENET_ATCR = ENET_ATCR_RESTART | ENET_ATCR_Reserved;  // Reset timer
   ENET_ATPER = NANOSECONDS_PER_SECOND;                 // Wrap at 10^9
   ENET_ATINC = ENET_ATINC_INC(NANOSECONDS_PER_SECOND / F_ENET_TS_CLK);
