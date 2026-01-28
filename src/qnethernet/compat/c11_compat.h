cf5bb97b038147573494710361af001ce6df5e14
Shawn Silverman <shawn@pobox.com>
2026-01-28 07:48:43 -0800

    c11: Update compatibility header for C++ >= 17 and add [[fallthrough]]

--- src/qnethernet/compat/c11_compat.h
+++ src/qnethernet/compat/c11_compat.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: (c) 2025 Shawn Silverman <shawn@pobox.com>
+// SPDX-FileCopyrightText: (c) 2025-2026 Shawn Silverman <shawn@pobox.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 // c11_compat.h defines some C11 compatibility things.
@@ -6,8 +6,10 @@
 
 #pragma once
 
-#if defined(__cplusplus) || (__STDC_VERSION__ >= 202311L)
+#if (__cplusplus >= 201703L) || (__STDC_VERSION__ >= 202311L)
 #define ATTRIBUTE_NODISCARD [[nodiscard]]
+#define ATTRIBUTE_FALLTHROUGH [[fallthrough]]
 #else
 #define ATTRIBUTE_NODISCARD
+#define ATTRIBUTE_FALLTHROUGH
 #endif  // >= C++23
