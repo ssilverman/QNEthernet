2a61880bf030b21eb92e589d81a19ea64e9e8c34
Shawn Silverman <shawn@pobox.com>
2026-01-28 13:45:05 -0800

    compat: Check for already-defined in the compatibility headers
    This is in case the other header also gets included.

--- src/qnethernet/compat/c++11_compat.h
+++ src/qnethernet/compat/c++11_compat.h
@@ -13,25 +13,55 @@ namespace qindesign {
 namespace network {
 namespace compat {
 
+// Check for definitions in case c11_compat.h was included before this one
+
 // Unknown attributes are not specified to be ignored before C++17
 // See: https://en.cppreference.com/w/cpp/language/attributes.html
 #if __cplusplus < 201703L
+#ifndef ATTRIBUTE_ALWAYS_INLINE
 #define ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
+#endif
+#ifndef ATTRIBUTE_DEPRECATED
 #define ATTRIBUTE_DEPRECATED(A) __attribute__((deprecated(A)))
+#endif
+#ifndef ATTRIBUTE_FALLTHROUGH
 #define ATTRIBUTE_FALLTHROUGH __attribute__((fallthrough))
-#define ATTRIBUTE_FORMAT(A, B, C) __attribute__((format (A, B, C)))
+#endif
+#ifndef ATTRIBUTE_FORMAT
+#define ATTRIBUTE_FORMAT(A, B, C) __attribute__((format(A, B, C)))
+#endif
+#ifndef ATTRIBUTE_MAYBE_UNUSED
 #define ATTRIBUTE_MAYBE_UNUSED __attribute__((unused))
+#endif
+#ifndef ATTRIBUTE_NODISCARD
 // It's hard to silence the warnings, even if casting a result to void
 #define ATTRIBUTE_NODISCARD /*__attribute__((warn_unused_result))*/
+#endif
+#ifndef ATTRIBUTE_WEAK
 #define ATTRIBUTE_WEAK __attribute__((weak))
+#endif
 #else
+#ifndef ATTRIBUTE_ALWAYS_INLINE
 #define ATTRIBUTE_ALWAYS_INLINE [[gnu::always_inline]]
+#endif
+#ifndef ATTRIBUTE_DEPRECATED
 #define ATTRIBUTE_DEPRECATED(A) [[deprecated(A)]]
+#endif
+#ifndef ATTRIBUTE_FALLTHROUGH
 #define ATTRIBUTE_FALLTHROUGH [[fallthrough]]
+#endif
+#ifndef ATTRIBUTE_FORMAT
 #define ATTRIBUTE_FORMAT(A, B, C) [[gnu::format(A, B, C)]]
+#endif
+#ifndef ATTRIBUTE_MAYBE_UNUSED
 #define ATTRIBUTE_MAYBE_UNUSED [[maybe_unused]]
+#endif
+#ifndef ATTRIBUTE_NODISCARD
 #define ATTRIBUTE_NODISCARD [[nodiscard]]
+#endif
+#ifndef ATTRIBUTE_WEAK
 #define ATTRIBUTE_WEAK [[gnu::weak]]
+#endif
 #endif  // C++ < 17
 
 // Creates a new std::unique_ptr.
--- src/qnethernet/compat/c11_compat.h
+++ src/qnethernet/compat/c11_compat.h
@@ -6,19 +6,45 @@
 
 #pragma once
 
+// Check for definitions in case c++11_compat.h was included before this one
+
 #if (__cplusplus >= 201703L) || (__STDC_VERSION__ >= 202311L)
+#ifndef ATTRIBUTE_ALWAYS_INLINE
 #define ATTRIBUTE_ALWAYS_INLINE [[gnu::always_inline]]
+#endif
+#ifndef ATTRIBUTE_DEPRECATED
 #define ATTRIBUTE_DEPRECATED(A) __attribute__((deprecated(A)))
+#endif
+#ifndef ATTRIBUTE_FALLTHROUGH
 #define ATTRIBUTE_FALLTHROUGH [[fallthrough]]
+#endif
+#ifndef ATTRIBUTE_MAYBE_UNUSED
 #define ATTRIBUTE_MAYBE_UNUSED [[maybe_unused]]
+#endif
+#ifndef ATTRIBUTE_NODISCARD
 #define ATTRIBUTE_NODISCARD [[nodiscard]]
+#endif
+#ifndef ATTRIBUTE_WEAK
 #define ATTRIBUTE_WEAK [[gnu::weak]]
+#endif
 #else
+#ifndef ATTRIBUTE_ALWAYS_INLINE
 #define ATTRIBUTE_ALWAYS_INLINE __attribute((always_inline))
+#endif
+#ifndef ATTRIBUTE_DEPRECATED
 #define ATTRIBUTE_DEPRECATED(A) [[deprecated(A)]]
+#endif
+#ifndef ATTRIBUTE_FALLTHROUGH
 #define ATTRIBUTE_FALLTHROUGH __attribute((fallthrough))
+#endif
+#ifndef ATTRIBUTE_MAYBE_UNUSED
 #define ATTRIBUTE_MAYBE_UNUSED __attribute__((unused))
+#endif
+#ifndef ATTRIBUTE_NODISCARD
 // It's hard to silence the warnings, even if casting a result to void
 #define ATTRIBUTE_NODISCARD /*__attribute__((warn_unused_result))*/
+#endif
+#ifndef ATTRIBUTE_WEAK
 #define ATTRIBUTE_WEAK __attribute__((weak))
+#endif
 #endif  // >= C++23
