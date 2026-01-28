5a9d2a76b0512b679cecdd11d309e499648251be
Shawn Silverman <shawn@pobox.com>
2026-01-28 12:32:16 -0800

    driver: Add a note to driver_restart_auto_negotiation() docs
    To see driver_get_capabilities(dc).

--- src/qnethernet/lwip_driver.h
+++ src/qnethernet/lwip_driver.h
@@ -246,6 +246,8 @@ void driver_notify_manual_link_state(bool flag);
 // --------------------------------------------------------------------------
 
 // Restarts auto-negotiation, if the driver supports it.
+//
+// See also: driver_get_capabilities(dc)
 void driver_restart_auto_negotiation();
 
 // --------------------------------------------------------------------------
