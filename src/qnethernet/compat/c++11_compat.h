53062ad84fa36685514372dfad06303e8964b26c
Shawn Silverman <shawn@pobox.com>
2026-01-28 11:59:45 -0800

    c++11: Add ATTRIBUTE_ALWAYS_INLINE to compatibility header

--- src/qnethernet/compat/c++11_compat.h
+++ src/qnethernet/compat/c++11_compat.h
@@ -16,16 +16,18 @@ namespace compat {
 // Unknown attributes are not specified to be ignored before C++17
 // See: https://en.cppreference.com/w/cpp/language/attributes.html
 #if __cplusplus < 201703L
+#define ATTRIBUTE_ALWAYS_INLINE __attribute__((always_inline))
 #define ATTRIBUTE_FALLTHROUGH
 #define ATTRIBUTE_NODISCARD
 #else
+#define ATTRIBUTE_ALWAYS_INLINE [[gnu::always_inline]]
 #define ATTRIBUTE_FALLTHROUGH [[fallthrough]]
 #define ATTRIBUTE_NODISCARD [[nodiscard]]
 #endif  // C++ < 17
 
 // Creates a new std::unique_ptr.
 template <typename T, typename... Args>
-[[gnu::always_inline]]
+ATTRIBUTE_ALWAYS_INLINE
 inline std::unique_ptr<T> make_unique(Args&&... args) {
 #if __cplusplus < 201402L
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
