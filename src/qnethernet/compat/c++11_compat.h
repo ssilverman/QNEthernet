c8983a95d223cf5b1f59c2141d15c849f7c7e33d
Shawn Silverman <shawn@pobox.com>
2026-02-15 21:03:27 -0800

    c++11: Improve the heading text in the compatibility header

--- src/qnethernet/compat/c++11_compat.h
+++ src/qnethernet/compat/c++11_compat.h
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: (c) 2025-2026 Shawn Silverman <shawn@pobox.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-// c++11_compat.h defines some C++11 compatibility functions.
+// c++11_compat.h defines some C++11 compatibility things.
 // This file is part of the QNEthernet library.
 
 #pragma once
