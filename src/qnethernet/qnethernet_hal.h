47429ec3c5ff839a8c4ed028b9a379c6ba26f9d5
Shawn Silverman <shawn@pobox.com>
2026-03-07 15:11:05 -0800

    hal: Add no-interrupts START and END macros for using exclusive monitors
    LDREXW and STREXW on Teensy platforms, and disable/enable interrupts
    on other platforms.

---
+++ src/qnethernet/qnethernet_hal.h
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: (c) 2026 Shawn Silverman <shawn@pobox.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// qnethernet_hal.h defines some useful HAL utilities.
+// This file is part of the QNEthernet library.
+
+// The QNETHERNET_HAL_START_NOINTERRUPTS_BLOCK() and
+// QNETHERNET_HAL_END_NOINTERRUPTS_BLOCK() macros either disable and enable
+// interrupts, or wrap code in a do-while block that utilizes LDREXW and STREXW.
+// In other words, everything between them may or may not be inside a block.
+// This means that any variables defined between them need to be declared
+// outside the pair of calls.
+//
+// This is useful for when a platform supports performing a task, but it's
+// preferred not to disable interrupts. For example, for lower latency
+// or efficiency.
+//
+// Usage example:
+//     QNETHERNET_HAL_START_NOINTERRUPTS_BLOCK() {
+//       perform_task();
+//     } QNETHERNET_HAL_END_NOINTERRUPTS_BLOCK();
+
+// TODO: Implemenet LDREX- and STREX-like usage for other platforms
+
+#if defined(TEENSYDUINO) &&                                 \
+    (defined(__IMXRT1062__) || defined(ARDUINO_TEENSY36) || \
+     defined(ARDUINO_TEENSY35) || defined(ARDUINO_TEENSY32))
+#include <stdint.h>
+
+#include <arm_math.h>  // For LDREXW and STREXW instructions
+
+#define QNETHERNET_HAL_START_NOINTERRUPTS_BLOCK() \
+  uint32_t _hal_nointerrupts_monitor;             \
+  do {                                            \
+    (void)__LDREXW(&_hal_nointerrupts_monitor);
+
+#define QNETHERNET_HAL_END_NOINTERRUPTS_BLOCK() \
+  } while (__STREXW(1, &_hal_nointerrupts_monitor) != 0)
+
+#else
+
+extern "C" {
+void qnethernet_hal_disable_interrupts();
+void qnethernet_hal_enable_interrupts();
+}  // extern "C"
+
+#define QNETHERNET_HAL_START_NOINTERRUPTS_BLOCK() \
+  qnethernet_hal_disable_interrupts();
+
+#define QNETHERNET_HAL_END_NOINTERRUPTS_BLOCK() \
+  qnethernet_hal_enable_interrupts()
+
+#endif  // Has xREXW instructions
