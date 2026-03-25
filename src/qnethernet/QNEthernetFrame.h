562744a7659f38719b7b79b1074b2b343de57148
Shawn Silverman <shawn@pobox.com>
2026-03-25 11:15:04 -0700

    EthernetFrame: Use full path for an include

--- src/qnethernet/QNEthernetFrame.h
+++ src/qnethernet/QNEthernetFrame.h
@@ -21,12 +21,12 @@
 #include "lwip/netif.h"
 #include "lwip/pbuf.h"
 #include "lwip/prot/ethernet.h"
-#include "lwip_driver.h"
 #include "qnethernet/StaticInit.h"
 #include "qnethernet/compat/c++11_compat.h"
 #include "qnethernet/internal/CircularBuffer.h"
 #include "qnethernet/internal/PrintfChecked.h"
 #include "qnethernet/internal/optional.h"
+#include "qnethernet/lwip_driver.h"
 
 extern "C" {
 err_t unknown_eth_protocol(struct pbuf* p, struct netif* netif);
