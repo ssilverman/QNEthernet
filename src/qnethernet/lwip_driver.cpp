ssilverman/QNEthernet

commit fb87d73811db7017eeb190e644c9882d1c7bc85f

project: Update some C-style casts to C++-style

src/qnethernet/lwip_driver.cpp
@@ -286,8 +286,10 @@ bool output_frame(const void* const frame, const size_t len) {
   }
 
   // Check length depending on VLAN
-  if ((((const uint8_t*)frame)[12] == (uint8_t)(ETHTYPE_VLAN >> 8)) &&
-      (((const uint8_t*)frame)[13] == (uint8_t)(ETHTYPE_VLAN))) {
+  if (((static_cast<const uint8_t*> frame)[12] ==
+       static_cast<uint8_t>(ETHTYPE_VLAN >> 8)) &&
+      ((static_cast<const uint8_t*> frame)[13] ==
+       static_cast<uint8_t>(ETHTYPE_VLAN))) {
     if (len < (6 + 6 + 2 + 2 + 2)) {  // dst + src + VLAN tag + VLAN info + len/type
       return false;
     }
@@ -305,12 +307,12 @@ bool output_frame(const void* const frame, const size_t len) {
   // Check for a loopback frame
   const bool isOurMAC = (std::memcmp(frame, s_mac, 6) == 0);
   if (isOurMAC || (std::memcmp(frame, kBroadcastMAC, 6) == 0)) {
-    struct pbuf* const p =
-        pbuf_alloc(PBUF_RAW, (uint16_t)(len + ETH_PAD_SIZE), PBUF_POOL);
+    struct pbuf* const p = pbuf_alloc(
+        PBUF_RAW, static_cast<uint16_t>(len + ETH_PAD_SIZE), PBUF_POOL);
     if (p != nullptr) {
-      LWIP_ASSERT(
-          "Expected space for pbuf fill",
-          pbuf_take_at(p, frame, (uint16_t)len, ETH_PAD_SIZE) == ERR_OK);
+      LWIP_ASSERT("Expected space for pbuf fill",
+                  pbuf_take_at(p, frame, static_cast<uint16_t>(len),
+                               ETH_PAD_SIZE) == ERR_OK);
       if (s_netif.input(p, &s_netif) != ERR_OK) {
         (void)pbuf_free(p);
       }

src/qnethernet/security/siphash.cpp
@@ -37,7 +37,7 @@ uint64_t siphash(const size_t c, const size_t d,
   uint64_t k0;
   uint64_t k1;
   (void)std::memcpy(&k0, key, 8);
-  (void)std::memcpy(&k1, (uint8_t*)key + 8, 8);
+  (void)std::memcpy(&k1, static_cast<const uint8_t*>(key) + 8, 8);
 #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
   k0 = __builtin_bswap64(k0);
   k1 = __builtin_bswap64(k1);
@@ -68,7 +68,7 @@ uint64_t siphash(const size_t c, const size_t d,
   }
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-  m = ((uint64_t)len << 56);
+  m = (static_cast<uint64_t>(len) << 56);
   (void)std::memcpy(&m, pMsg, len % 8);
 #else
   m = len & 0xff;

test/test_util/test_main.cpp
@@ -111,7 +111,8 @@ static void test_StdioPrint() {
 static void test_NullPrint() {
   NullPrint np;
   TEST_ASSERT_EQUAL_MESSAGE(1, np.write(1), "Expected byte written");
-  TEST_ASSERT_EQUAL_MESSAGE(12, np.write((void*)nullptr, 12), "Expected bytes written");
+  TEST_ASSERT_EQUAL_MESSAGE(12, np.write(static_cast<const void*>(nullptr), 12),
+                            "Expected bytes written");
   TEST_ASSERT_EQUAL_MESSAGE(std::numeric_limits<int>::max(),
                             np.availableForWrite(),
                             "Expected max. bytes available to write");

commit 03133117e7b63137973224726f72c72be7ac131a

driver: Fix some parentheses errors
Was introduced in fb87d7381.

src/qnethernet/lwip_driver.cpp
@@ -286,10 +286,10 @@ bool output_frame(const void* const frame, const size_t len) {
   }
 
   // Check length depending on VLAN
-  if (((static_cast<const uint8_t*> frame)[12] ==
+  if ((static_cast<const uint8_t*>(frame)[12] ==
        static_cast<uint8_t>(ETHTYPE_VLAN >> 8)) &&
-      ((static_cast<const uint8_t*> frame)[13] ==
-       static_cast<uint8_t>(ETHTYPE_VLAN))) {
+      ((static_cast<const uint8_t*>(frame)[13] ==
+        static_cast<uint8_t>(ETHTYPE_VLAN)))) {
     if (len < (6 + 6 + 2 + 2 + 2)) {  // dst + src + VLAN tag + VLAN info + len/type
       return false;
     }