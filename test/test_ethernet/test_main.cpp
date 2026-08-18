9aae73b199c3dd70f756bfc61e9aacceddd15e6b
Shawn Silverman <shawn@pobox.com>
2026-08-17 19:27:02 -0700

    test_ethernet: Improve test_udp() with NTP changes from the old SNTPClient

--- test/test_ethernet/test_main.cpp
+++ test/test_ethernet/test_main.cpp
@@ -57,8 +57,8 @@ std::vector<char> format(const char* format, Args... args) {
 // Timeouts
 static constexpr uint32_t kDHCPTimeout = 30000;
 static constexpr uint32_t kLinkTimeout = 5000;
-static constexpr uint32_t kSNTPTimeout = 10000;
-static constexpr uint32_t kSNTPResendTimeout = 2500;
+static constexpr uint32_t kNTPTimeout  = 10000;
+static constexpr uint32_t kNTPResendTimeout = 2500;
 static constexpr uint32_t kConnectTimeout = 10000;
 
 // Default static IP configuration
@@ -723,62 +723,95 @@ static void test_interface_listener() {
   TEST_ASSERT_FALSE_MESSAGE(Ethernet.interfaceStatus(), "Expected interface down");
 }
 
-// Tests UDP by using SNTP.
+// 01-Jan-1900 00:00:00 -> 01-Jan-1970 00:00:00
+static constexpr int64_t kEpochDiff = 2208988800;
+
+// 2^32
+static constexpr double k2e32 = double{uint64_t{1} << 32};
+
+// Converts an NTP timestamp to a double. The pivot helps determine the era and
+// do proper wraparound.
+static double ntpToDouble(const uint8_t* const ts, const double pivot) {
+  uint32_t secs;
+  uint32_t frac;
+  std::memcpy(&secs, &ts[0], 4);
+  std::memcpy(&frac, &ts[4], 4);
+  secs = ntohl(secs);
+  frac = ntohl(frac);
+
+  double t = static_cast<double>(secs) + (static_cast<double>(frac)/k2e32);
+
+  // Choose the NTP era closest to the known local request time.
+  return t + std::round((pivot - t)/k2e32)*k2e32;
+}
+
+// Retrieves the current NTP-based time, returning a quiet NaN if there was an
+// error. This will also fill a non-NULL 'raw' with the raw timestamp bytes.
+static double currentTime(uint8_t* const raw) {
+  ::timeval tv;
+  if (::gettimeofday(&tv, nullptr) != 0) {
+    // Something is wrong. This should always return zero.
+    printf("Failed to get current time: errno=%d\r\n", errno);
+    return std::numeric_limits<double>::quiet_NaN();
+  }
+
+  const auto secs = static_cast<int64_t>(tv.tv_sec) + kEpochDiff;
+  const auto frac = static_cast<double>(tv.tv_usec) * 1e-6;
+
+  if (raw != nullptr) {
+    const uint32_t net_secs = htonl(static_cast<uint32_t>(secs));
+    const uint32_t net_frac = htonl(static_cast<uint32_t>(frac * k2e32));
+
+    (void)std::memcpy(&raw[0], &net_secs, 4);
+    (void)std::memcpy(&raw[4], &net_frac, 4);
+  }
+
+  return static_cast<double>(secs) + frac;
+}
+
+// Tests UDP by using NTP.
 static void test_udp() {
-  // 01-Jan-1900 00:00:00 -> 01-Jan-1970 00:00:00
-  constexpr std::time_t kEpochDiff = 2208988800;
-
-  // Epoch -> 07-Feb-2036 06:28:16
-  constexpr std::time_t kBreakTime = 2085978496;
-
   constexpr uint16_t kNTPPort = 123;
 
   if (!waitForLocalIP()) {
     return;
   }
 
-  uint8_t buf[48];
-  (void)std::fill_n(buf, 48, 0);
+  std::array<uint8_t, 48> packet{0};
 #if __cplusplus < 201402L
-  buf[0] = 0x23;
+  packet[0] = 0x23;
 #else
-  buf[0] = 0b00'100'011;  // LI=0, VN=4, Mode=3 (Client)
+  packet[0] = 0b00'100'011;  // LI=0, VN=4, Mode=3 (Client)
 #endif  // C++ < 14
 
-  // Set the Transmit Timestamp
-  std::time_t t = std::time(nullptr);
-  if (t >= kBreakTime) {
-    t -= kBreakTime;
-  } else {
-    t += kEpochDiff;
+  // Get the current time as the Transmit Timestamp
+  if (std::isnan(currentTime(&packet[40]))) {
+    TEST_FAIL_MESSAGE("Could not get current time");
+    return;
   }
-  buf[40] = t >> 24;
-  buf[41] = t >> 16;
-  buf[42] = t >> 8;
-  buf[43] = t;
 
   // Send the packet
   udp = compat::make_unique<EthernetUDP>();
-  TEST_MESSAGE("Listening on SNTP port...");
+  TEST_MESSAGE("Listening on NTP port...");
   TEST_ASSERT_TRUE_MESSAGE(udp->begin(kNTPPort), "Expected UDP listen success");
 
   bool validReply = false;
-  uint32_t sntpTime = 0;
+  double t3 = 0;
   const uint32_t timer = millis();
-  uint32_t resendTimer = millis() + kSNTPResendTimeout;
+  uint32_t resendTimer = timer + kNTPResendTimeout;
   bool first = true;
 
-  while ((millis() - timer) < kSNTPTimeout) {
-    // Do SNTP resends
-    if ((millis() - resendTimer) >= kSNTPResendTimeout) {
+  while ((millis() - timer) < kNTPTimeout) {
+    // Do NTP resends
+    if ((millis() - resendTimer) >= kNTPResendTimeout) {
       if (first) {
-        TEST_MESSAGE("Sending SNTP request...");
+        TEST_MESSAGE("Sending NTP request...");
         first = false;
       } else {
-        TEST_MESSAGE("Resending SNTP request...");
+        TEST_MESSAGE("Resending NTP request...");
       }
       TEST_ASSERT_TRUE_MESSAGE(
-          udp->send(Ethernet.gatewayIP(), kNTPPort, buf, 48),
+          udp->send(Ethernet.gatewayIP(), kNTPPort, packet.data(), 48),
           "Expected UDP send success");
       resendTimer = millis();
     }
@@ -789,7 +822,14 @@ static void test_udp() {
     if (size < 0) {
       continue;
     }
-    if ((size != 48) && (size != 68)) {
+
+    // Arrival Timestamp
+    const double t4 = currentTime(nullptr);
+    if (std::isnan(t4)) {
+      return;
+    }
+
+    if (((size != 48) && (size != 68)) || (udp->remotePort() != kNTPPort)) {
       TEST_MESSAGE("Discarding incorrect-sized reply");
       continue;
     }
@@ -797,48 +837,53 @@ static void test_udp() {
     const uint8_t* const data = udp->data();
 
     // See: Section 5, "SNTP Client Operations"
-    const int mode = data[0] & 0x07;
-    if (((data[0] & 0xc0) == 0xc0) ||     // LI == 3 (Alarm condition)
-        (data[1] == 0) ||                 // Stratum == 0 (Kiss-o'-Death)
-        !((mode == 4) || (mode == 5))) {  // Must be Server or Broadcast mode
-      TEST_MESSAGE("Discarding SNTP reply");
+    const unsigned int mode    = data[0] & 0x07u;
+    const unsigned int li      = (data[0] & 0xc0u) >> 6;
+    const unsigned int version = (data[0] & 0x38u) >> 3;
+
+    if ((li == 3) ||                          // LI == 3 (Alarm condition)
+        (data[1] == 0) || (data[1] >= 16) ||  // Stratum == 0 (Kiss-o'-Death)
+        !((mode == 4) || (mode == 5)) ||      // Must be Server or Broadcast mode
+        (version != 4)) {
+      TEST_MESSAGE("Discarding NTP reply");
       continue;
     }
 
-    sntpTime = (uint32_t{data[40]} << 24) |
-               (uint32_t{data[41]} << 16) |
-               (uint32_t{data[42]} << 8) |
-               uint32_t{data[43]};
-    if (sntpTime == 0) {
-      TEST_MESSAGE("Discarding SNTP reply: timestamp is zero");
+    if (std::all_of(&data[40], &data[44],
+                    [](const uint8_t b) { return b == 0; })) {
+      TEST_MESSAGE("Discarding NTP reply: timestamp is zero");
       continue;
     }
+
+    t3 = ntpToDouble(&data[40], t4);
 
     validReply = true;
     break;
   }
+
 
   TEST_ASSERT_TRUE_MESSAGE(validReply, "Expected valid reply");
   if (!validReply) {
     return;
   }
 
-  TEST_MESSAGE(format("SNTP reply time: %" PRIu32 "ms",
-                      (millis() - timer)).data());
+  // Transmit Timestamp
+  double newTime = t3 - static_cast<double>(kEpochDiff);
+  // Note: the new time should also add the one-way delay, if known or estimated
 
-  if ((sntpTime & 0x80000000U) == 0) {
-    // See: Section 3, "NTP Timestamp Format"
-    sntpTime += kBreakTime;
-  } else {
-    sntpTime -= kEpochDiff;
-  }
+  TEST_MESSAGE(
+      format("NTP reply time: %" PRIu32 "ms", (millis() - timer)).data());
 
   // Print the time
-  const std::time_t time = sntpTime;
-  const std::tm* const tm = std::gmtime(&time);
-  TEST_MESSAGE(format("SNTP reply: %04u-%02u-%02u %02u:%02u:%02u (UTC)",
+  const auto secs = static_cast<time_t>(newTime);
+  const auto usecs =
+      static_cast<suseconds_t>((newTime - static_cast<double>(secs))*1e6);
+  const std::tm* const tm = std::gmtime(&secs);
+  TEST_MESSAGE(format("NTP reply: %04u-%02u-%02u %02u:%02u:%02u"
+                      " (%" PRId32 " µs) UTC",
                       tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-                      tm->tm_hour, tm->tm_min, tm->tm_sec).data());
+                      tm->tm_hour, tm->tm_min, tm->tm_sec,
+                      usecs).data());
 }
 
 // Tests UDP receive queueing.
